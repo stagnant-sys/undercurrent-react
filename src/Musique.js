e7b1db5a665f6165b952387a7ff241882c0f59e6
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-05-02 22:04:54 +0200

    Working article structure

--- src/Musique.js
+++ src/Musique.js
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Article from "./components/Article";
 
 const Musique = () => {
+  const [articles, setArticles] = useState([
+    {
+      category: "musique",
+      id: "mus1",
+      src: "https://e.snmc.io/i/fullres/w/99da79efac5f730677513c630e6be9b5/6279815",
+      artist: "Chatmonchy",
+      release: "耳鳴り (Miminari)",
+      subtitle: "Chatmonchy's first album",
+      content: "This is the first album from Chatmonchy.",
+      published: "Today",
+
+    },
+    {
+      category: "musique",
+      id: "mus2",
+      src: "https://e.snmc.io/i/fullres/w/11a369ffd607c17079e4c3459a6325a2/2278249",
+      artist: "Chatmonchy",
+      release: "生命力 (Seimeiryoku)",
+      subtitle: "Chatmonchy's second album",
+      content: "This is the second album from Chatmonchy.",
+      published: "Today"
+    }
+  ]);
+
+  const articlesList = articles.map((el) => 
+    <Article 
+      category={el.category}
+      id={el.id}
+      src={el.src}
+      artist={el.artist}
+      release={el.release}
+      subtitle={el.subtitle}
+      content={el.content}
+    />
+    );
+
   return (
     <div>
-      <Article 
-        id="test" 
-        src="null" 
-        artist="Chatmonchy" 
-        release="耳鳴り (Miminari)"
-        subtitle="Chatmonchy's first album"
-        content="This is the first album from Chatmonchy."
-      />
+      {articlesList}
     </div>
   )
 }
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,18 +1,29 @@
 import React from "react";
 import "./article.css";
 
-const Article = ({ id, src, artist, release, subtitle, content }) => {
+const Article = ({ category, id, src, artist, release, subtitle, content }) => {
+  const CategoryBadge = () => {
+    if (category === "musique") {
+      return <div className="card-category musique-category">musique</div>
+    } else if (category === "jv") {
+      return <div className="card-category jv-category">jeu vidéo</div>
+    } else if (category === "livre") {
+      return <div className="card-category livre-category">livre</div>
+    }
+  };
+
+
   return (
     <div id={id} className="content-card">
-      <div className="card-category musique-category">Musique</div>
+      <CategoryBadge />
       <img className="content-card__image" src={src} />
       <div className="content-card__title">
         {artist} - {release}
       </div>
-      <div class="content-card__subtitle">
+      <div className="content-card__subtitle">
         {subtitle}
       </div>
-      <div class="content-card__text">
+      <div className="content-card__text">
         <div>{content}</div>
       </div>
     </div>
